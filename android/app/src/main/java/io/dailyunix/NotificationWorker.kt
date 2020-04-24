8e7558a4288fd37e7d58c5eaa0d155093fb146f4
Eliot Stock <1@eliotstock.com>
2020-04-24 13:55:22 +0100

    Run at 08:45 daily.

--- android/app/src/main/java/io/dailyunix/NotificationWorker.kt
+++ android/app/src/main/java/io/dailyunix/NotificationWorker.kt
@@ -10,19 +10,43 @@ import androidx.concurrent.futures.CallbackToFutureAdapter;
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.google.common.util.concurrent.ListenableFuture
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 private const val channelId = "daily"
 
 // Package level function
 fun reschedule(appContext: Context) {
-    val workRequest = PeriodicWorkRequest.Builder(NotificationWorker::class.java, 24,
-        TimeUnit.HOURS).build()
-    WorkManager.getInstance(appContext).enqueueUniquePeriodicWork(
-            NotificationWorker::class.java.name, ExistingPeriodicWorkPolicy.REPLACE, workRequest)
+    // Thanks: https://medium.com/androiddevelopers/workmanager-periodicity-ff35185ff006
+    val dueDate = Calendar.getInstance()
+    val currentDate = Calendar.getInstance()
+
+    // Set Execution around 08:45:00
+    dueDate.set(Calendar.HOUR_OF_DAY, 8)
+    dueDate.set(Calendar.MINUTE, 45)
+    dueDate.set(Calendar.SECOND, 0)
+
+    if (dueDate.before(currentDate)) {
+        dueDate.add(Calendar.HOUR_OF_DAY, 24)
+    }
+
+    val timeDiff = dueDate.timeInMillis - currentDate.timeInMillis
+
+    val dailyWorkRequest = OneTimeWorkRequest.Builder(NotificationWorker::class.java)
+        .setInitialDelay(timeDiff, TimeUnit.MILLISECONDS)
+        .addTag(NotificationWorker::class.java.name)
+        .build()
+
+    WorkManager.getInstance(appContext).enqueue(dailyWorkRequest)
+
+//    val workRequest = PeriodicWorkRequest.Builder(NotificationWorker::class.java, 24,
+//        TimeUnit.HOURS).build()
+//    WorkManager.getInstance(appContext).enqueueUniquePeriodicWork(
+//            NotificationWorker::class.java.name, ExistingPeriodicWorkPolicy.REPLACE, workRequest)
 }
 
-// TODO (P1): Call this exactly once in the lifetime of the application.
+// TODO (P1): Call this exactly once in the lifetime of the application, such as when the welcome
+//  activity is shown.
 fun createNotificationChannel(appContext: Context) {
     // Create the NotificationChannel, but only on API 26+ because
     // the NotificationChannel class is new and not in the support library
@@ -42,7 +66,7 @@ fun createNotificationChannel(appContext: Context) {
 }
 
 fun showNotification(appContext: Context, command: String, whatis: String) {
-    var builder = NotificationCompat.Builder(appContext, channelId)
+    val builder = NotificationCompat.Builder(appContext, channelId)
         .setSmallIcon(R.drawable.ic_launcher_foreground) // TODO (P2): Notification icon
         .setContentTitle("Daily Unix Command")
         .setContentText("$command. $whatis")
